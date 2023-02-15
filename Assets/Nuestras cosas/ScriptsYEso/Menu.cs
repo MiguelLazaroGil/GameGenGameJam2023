987087591f217f71a5adaf21fa42bbd3751adb95
Miguel Lazaro Gil <lazaro.gil.miguel@gmail.com>
2023-02-15 20:17:28 +0100

    MenuPuestoDelTodo

--- Assets/Nuestras cosas/ScriptsYEso/Menu.cs
+++ Assets/Nuestras cosas/ScriptsYEso/Menu.cs
@@ -8,6 +8,8 @@ public class Menu : MonoBehaviour
 {
     public Button play;
     public Button exit;
+    public Button exitSi;
+    public Button exitNo;
     public RectTransform ExitUI;
 
     // Start is called before the first frame update
@@ -16,6 +18,8 @@ public class Menu : MonoBehaviour
         ExitUI.gameObject.SetActive(false);
         play.onClick.AddListener(PlayButtonClicked);
         exit.onClick.AddListener(ExitButtonClicked);
+        exitSi.onClick.AddListener(Salir_Si);
+        exitNo.onClick.AddListener(Salir_No);
     }
 
     private void PlayButtonClicked() {
@@ -24,5 +28,12 @@ public class Menu : MonoBehaviour
 
     private void ExitButtonClicked() {
         ExitUI.gameObject.SetActive(true);
+    }
+    public void Salir_Si(){
+        Application.Quit(0);
+    }
+    public void Salir_No(){
+        ExitUI.gameObject.SetActive(false);
+
     }
 }
